145b2c552372ef4b1f5d0de80a274134cfe6b3cb
alienx5499 <p2005p5499p@gmail.com>
2026-01-13 15:05:00 +0530

    initialize backend

---
+++ backend/src/test/java/com/shopifyr/backend/ShopifyrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopifyr.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopifyrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
